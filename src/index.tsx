5420478cbfa1b035d6d7036a568f3ad8417e31e3
Евгений Мищенко <evgenjion@yandex-team.ru>
2020-04-22 19:17:54 +0300

    Pretify Homepage

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import 'typeface-roboto';
+
+import './index.css';
 
 import { rootReducer } from './store';
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { privateRouteComponent } from "../../components/hoc/privateRouteComponent";
-import { Grid, makeStyles, Theme, CircularProgress } from "@material-ui/core";
+import { Grid, makeStyles, Theme, CircularProgress, ListItem, Link } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
 import { IRootState } from "../../store";
 import { allArticlesLoad } from "../../store/articles/actions";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) => ({
     main: {
         padding: theme.spacing(4),
     },
+    li: {
+        listStyleType: 'none'
+    }
 }));
 
 function HomePage() {
@@ -31,7 +34,7 @@ function HomePage() {
             className={classes.main}
             >
             <Grid item xs>
-                <h3>Hello {userName}!</h3>
+                <h2>Hello {userName}!</h2>
 
                 {loaded ?
                     (<>
@@ -39,10 +42,15 @@ function HomePage() {
                         <ul>
                             {Object.values(articleList).map(({id, title}) => {
                                 return (
-                                    <li key={`article-${id}`}>
-                                        <Link to={`/article/${id}`}>
-                                            {title}
-                                        </Link>
+                                    <li key={`article-${id}`} className={classes.li}>
+                                        <NavLink to={`/article/${id}`}>
+                                            <ListItem button>
+                                                <Link component="button" variant="body1">{title}</Link>
+                                                {/* <ListItemText primary={title} /> */}
+                                            </ListItem>
+                                            {/* {title} */}
+                                        </NavLink>
+
                                     </li>
                                 )
                             })}
